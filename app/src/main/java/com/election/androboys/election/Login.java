b185dd302a01e529bfd82b2200821bc5ee1c474c
sk17ab <sk17ab@gmail.com>
2016-03-09 19:37:50 +0600

    Reaz

--- app/src/main/java/com/election/androboys/election/Login.java
+++ app/src/main/java/com/election/androboys/election/Login.java
@@ -62,7 +62,18 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num= Integer.parseInt(nid.getText().toString());
 
+                String num1=birth.getText().toString();
+
+
+
+                new HttpAsyncTask().execute("http://seekingsoft.com/android/election/url.php?action=register&nid="+num+"&birthday="+num1);
+            }
+        });
 
 
 //      btn_enter.setOnClickListener(new View.OnClickListener() {
